0a19f79a29b97385c3df4129340cfec85df12273
Jonathan W <jwilliams12117@gmail.com>
2023-07-25 22:25:09 -0700

    Added styled info bar section

--- playbank/app/page.jsx
+++ playbank/app/page.jsx
@@ -3,21 +3,24 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <div>
-          This is the info bar.
-          <div>
-            Current balance: $
-          </div>
-          <div>
-            Current date: Date
-          </div>
-          <div>
-            <button>
-              Transact
-            </button>
-          </div>
+      <div id="infobar" className='w-full flex items-center justify-center py-4 text-2xl border-b-4 border-gray-500'>
+        <div className='w-1/3 text-center'>
+          {(new Date()).toLocaleDateString("en-US")}
         </div>
+        <div className='w-1/3 text-center'>
+          Balance: $
+        </div>
+        <div className='w-1/3 text-center'>
+          <button
+            id="transactButton"
+            className='border border-emerald-500 rounded-lg bg-emerald-400 p-3 shadow-lg'
+            onClick={console.log("Clicked transact button")}
+          >
+            Transact
+          </button>
+        </div>
+        {/* Hidden Modal Transaction Form Section */}
+        {/* Transaction History Section */}
       </div>
     </main>
   )
